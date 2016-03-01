c873c0d89f1513b62d9c64cccc332588c7010a57
Peter Tiedemann <pt@configit.com>
2016-03-01 12:21:27 +0100

    Add attribute remover

--- src/RoslynSolutionTest/Program.cs
+++ src/RoslynSolutionTest/Program.cs
@@ -1,15 +1,29 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.MSBuild;
 
 namespace RoslynSolutionTest {
     class Program {
+        public static Solution RemoveAttributes( Solution solution ) {
+            foreach ( var doc in solution.Projects.SelectMany( p => p.Documents ).Where( d => d.SupportsSyntaxTree ) ) {
+                var tree = doc.GetSyntaxTreeAsync().Result;
+
+                var newDoc = doc.WithSyntaxRoot( new RemoveAttributesVisitor( new[] { "Flags" } ).Visit( tree.GetRoot() ) );
+                File.WriteAllText( newDoc.FilePath, newDoc.GetTextAsync().Result.ToString() );
+
+                solution = newDoc.Project.Solution;
+
+            }
+
+            return solution;
+        }
+
         static void Main( string[] args ) {
 
             var ws = MSBuildWorkspace.Create();
@@ -24,16 +38,22 @@ namespace RoslynSolutionTest {
                 solution = ws.OpenSolutionAsync( args[0] ).Result;
             }
 
-            foreach ( var document in solution.Projects.SelectMany( p => p.Documents.Where( d => d.SupportsSemanticModel ) ) ) {
-                Console.WriteLine( "Checking " + document.FilePath );
-                var model = document.GetSemanticModelAsync().Result;
-                var diag = model.GetDiagnostics();
-                if ( diag.Any( d => d.Severity == DiagnosticSeverity.Error ) ) {
-                    throw new Exception( string.Join( "\n", diag.Where( e => e.Severity == DiagnosticSeverity.Error ) ) );
-                }
-            }
+            solution = RemoveAttributes( solution );
 
-            Console.WriteLine( "No errors encountered" );
-        }
+            //foreach ( var doc in solution.Projects.SelectMany( p => p.Documents ).Where( d => d.SupportsSyntaxTree ) ) {
+            //    File.WriteAllText( doc.FilePath, doc.GetTextAsync().Result.ToString() );
+            //}
+
+                //foreach ( var document in solution.Projects.SelectMany( p => p.Documents.Where( d => d.SupportsSemanticModel ) ) ) {
+                //    Console.WriteLine( "Checking " + document.FilePath );
+                //    var model = document.GetSemanticModelAsync().Result;
+                //    var diag = model.GetDiagnostics();
+                //    if ( diag.Any( d => d.Severity == DiagnosticSeverity.Error ) ) {
+                //        throw new Exception( string.Join( "\n", diag.Where( e => e.Severity == DiagnosticSeverity.Error ) ) );
+                //    }
+                //}
+
+                //Console.WriteLine( "No errors encountered" );
+            }
     }
 }
---
+++ src/RoslynSolutionTest/RemoveAttributesVisitor.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace RoslynSolutionTest {
+    internal class RemoveAttributesVisitor: CSharpSyntaxRewriter {
+
+        private readonly HashSet<string> _keepAttrs = new HashSet<string>();
+
+        public RemoveAttributesVisitor( IEnumerable<string> keepAttrs ) {
+            foreach ( var i in keepAttrs ) {
+                _keepAttrs.Add( i );
+            }
+        }
+
+        // There is no base type or interface that can provide WithAttributes and AttributeLists....
+        private T ClearAttributesAndMoveTrivia<T>(
+            T node,
+            Func<T, SyntaxList<AttributeListSyntax>> attributeLists,
+            Func<T, SyntaxList<AttributeListSyntax>, T> withAttributes ) where T : SyntaxNode {
+            if ( !attributeLists( node ).Any() ) {
+                return node;
+            }
+
+            var leadingTrivia = attributeLists( node ).SelectMany( al => al.GetLeadingTrivia().Add( SyntaxFactory.CarriageReturnLineFeed ) );
+
+            var attrLists = attributeLists( node ).Select( attrList =>
+                SyntaxFactory.AttributeList( new SeparatedSyntaxList<AttributeSyntax>().AddRange(
+                    attrList.Attributes.Where( a => _keepAttrs.Contains( a.ToString() ) ) ) ) );
+
+            node = withAttributes( node, new SyntaxList<AttributeListSyntax>().AddRange( attrLists.Where( al => al.Attributes.Any() ) ) );
+
+            var updatedTrivia = SyntaxFactory.TriviaList( leadingTrivia ).AddRange( node.GetLeadingTrivia() );
+
+            return node.WithLeadingTrivia( updatedTrivia );
+        }
+
+        public override SyntaxNode VisitAttributeList( AttributeListSyntax node ) {
+            Debug.Assert( node.Parent.IsKind( SyntaxKind.CompilationUnit ) );
+            return node;
+        }
+
+        public override SyntaxNode VisitMethodDeclaration( MethodDeclarationSyntax node ) {
+            return ClearAttributesAndMoveTrivia( node, n => n.AttributeLists, ( n, a ) => n.WithAttributeLists( a ) );
+        }
+
+        public override SyntaxNode VisitPropertyDeclaration( PropertyDeclarationSyntax node ) {
+            return ClearAttributesAndMoveTrivia( node, n => n.AttributeLists, ( n, a ) => n.WithAttributeLists( a ) );
+        }
+
+        public override SyntaxNode VisitFieldDeclaration( FieldDeclarationSyntax node ) {
+            return ClearAttributesAndMoveTrivia( node, n => n.AttributeLists, ( n, a ) => n.WithAttributeLists( a ) );
+        }
+
+        public override SyntaxNode VisitDelegateDeclaration( DelegateDeclarationSyntax node ) {
+            return ClearAttributesAndMoveTrivia( node, n => n.AttributeLists, ( n, a ) => n.WithAttributeLists( a ) );
+        }
+
+        public override SyntaxNode VisitConstructorDeclaration( ConstructorDeclarationSyntax node ) {
+            return ClearAttributesAndMoveTrivia( node, n => n.AttributeLists, ( n, a ) => n.WithAttributeLists( a ) );
+        }
+
+        public override SyntaxNode VisitDestructorDeclaration( DestructorDeclarationSyntax node ) {
+            return ClearAttributesAndMoveTrivia( node, n => n.AttributeLists, ( n, a ) => n.WithAttributeLists( a ) );
+        }
+
+        public override SyntaxNode VisitConversionOperatorDeclaration( ConversionOperatorDeclarationSyntax node ) {
+            return ClearAttributesAndMoveTrivia( node, n => n.AttributeLists, ( n, a ) => n.WithAttributeLists( a ) );
+        }
+
+        public override SyntaxNode VisitOperatorDeclaration( OperatorDeclarationSyntax node ) {
+            return ClearAttributesAndMoveTrivia( node, n => n.AttributeLists, ( n, a ) => n.WithAttributeLists( a ) );
+        }
+
+        public override SyntaxNode VisitIndexerDeclaration( IndexerDeclarationSyntax node ) {
+            return ClearAttributesAndMoveTrivia( node, n => n.AttributeLists, ( n, a ) => n.WithAttributeLists( a ) );
+        }
+
+        public override SyntaxNode VisitEventDeclaration( EventDeclarationSyntax node ) {
+            return ClearAttributesAndMoveTrivia( node, n => n.AttributeLists, ( n, a ) => n.WithAttributeLists( a ) );
+        }
+
+        public override SyntaxNode VisitEventFieldDeclaration( EventFieldDeclarationSyntax node ) {
+            return ClearAttributesAndMoveTrivia( node, n => n.AttributeLists, ( n, a ) => n.WithAttributeLists( a ) );
+        }
+
+        public override SyntaxNode VisitEnumMemberDeclaration( EnumMemberDeclarationSyntax node ) {
+            return ClearAttributesAndMoveTrivia( node, n => n.AttributeLists, ( n, a ) => n.WithAttributeLists( a ) );
+        }
+
+        public override SyntaxNode VisitEnumDeclaration( EnumDeclarationSyntax node ) {
+            var enumDecl = ClearAttributesAndMoveTrivia( node, n => n.AttributeLists, ( n, a ) => n.WithAttributeLists( a ) );
+
+            return enumDecl.WithMembers( new SeparatedSyntaxList<EnumMemberDeclarationSyntax>().AddRange( 
+                enumDecl.Members.Select( m => (EnumMemberDeclarationSyntax) Visit( m ) ) ) );
+        }
+
+        public override SyntaxNode VisitClassDeclaration( ClassDeclarationSyntax node ) {
+            var classDecl = ClearAttributesAndMoveTrivia( node, n => n.AttributeLists, ( n, a ) => n.WithAttributeLists( a ) );
+
+            return base.VisitClassDeclaration( classDecl );
+        }
+
+        public override SyntaxNode VisitStructDeclaration( StructDeclarationSyntax node ) {
+            var structDecl = ClearAttributesAndMoveTrivia( node, n => n.AttributeLists, ( n, a ) => n.WithAttributeLists( a ) );
+
+            return base.VisitStructDeclaration( structDecl );
+        }
+
+        public override SyntaxNode VisitInterfaceDeclaration( InterfaceDeclarationSyntax node ) {
+            var decl = ClearAttributesAndMoveTrivia( node, n => n.AttributeLists, ( n, a ) => n.WithAttributeLists( a ) );
+
+            return base.VisitInterfaceDeclaration( decl );
+        }
+    }
+}
